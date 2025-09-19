feb23393fa5f57737ed481665add0a893bbc713c
15506957109 <2284467180@qq.com>
2025-09-19 11:33:39 +0800

    p99

--- daijia-parent/service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
+++ daijia-parent/service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
@@ -2,12 +2,19 @@ package com.atguigu.daijia.map.service.impl;
 
 import com.atguigu.daijia.common.constant.RedisConstant;
 import com.atguigu.daijia.map.service.LocationService;
+import com.atguigu.daijia.model.form.map.SearchNearByDriverForm;
 import com.atguigu.daijia.model.form.map.UpdateDriverLocationForm;
+import com.atguigu.daijia.model.vo.map.NearByDriverVo;
+import io.lettuce.core.api.sync.RedisGeoCommands;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.geo.Circle;
+import org.springframework.data.geo.GeoResults;
 import org.springframework.data.geo.Point;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -30,6 +37,24 @@ public class LocationServiceImpl implements LocationService {
     public Boolean removeDriverLocation(Long driverId) {
         redisTemplate.opsForGeo().remove(RedisConstant.DRIVER_GEO_LOCATION, driverId.toString());
         return true;
+    }
+
+    @Override
+    public List<NearByDriverVo> searchNearByDriver(SearchNearByDriverForm searchNearByDriverForm) {
+        // 搜索经纬度位置5公里以内的司机
+
+        // 1. 操作redis里面geo
+        // 1.1 穿件circle对象
+        Circle circle = null;
+        // 1.2 定义GEO参数
+        RedisGeoCommands.GeoRadiusCommandArgs args = null;
+        GeoResults<RedisGeoCommands.GeoLocation<String>> radius = redisTemplate.opsForGeo().radius(RedisConstant.DRIVER_GEO_LOCATION, circle, args);
+        // 2. 查询redis最后返回list集合
+
+        // 3. 对查询list集合进行处理
+        // 3.1 遍历list集合，得到每个司机信息
+        // 3.2 根据每个司机个性化设置信息判断
+        return null;
     }
 
 }
