58654770a1ddf5fd3a4f56753f0caf7797fbdaf7
Gxf <2284467180@qq.com>
2025-09-22 00:03:53 +0800

    p105 xxx-job调度下

--- daijia-parent/service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
+++ daijia-parent/service/service-map/src/main/java/com/atguigu/daijia/map/service/impl/LocationServiceImpl.java
@@ -1,20 +1,28 @@
 package com.atguigu.daijia.map.service.impl;
 
 import com.atguigu.daijia.common.constant.RedisConstant;
+import com.atguigu.daijia.common.constant.SystemConstant;
+import com.atguigu.daijia.common.result.Result;
+import com.atguigu.daijia.driver.client.DriverInfoFeignClient;
 import com.atguigu.daijia.map.service.LocationService;
+import com.atguigu.daijia.model.entity.driver.DriverSet;
 import com.atguigu.daijia.model.form.map.SearchNearByDriverForm;
 import com.atguigu.daijia.model.form.map.UpdateDriverLocationForm;
 import com.atguigu.daijia.model.vo.map.NearByDriverVo;
-import io.lettuce.core.api.sync.RedisGeoCommands;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.geo.Circle;
-import org.springframework.data.geo.GeoResults;
-import org.springframework.data.geo.Point;
+import org.springframework.data.geo.*;
+import org.springframework.data.redis.connection.RedisGeoCommands;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -23,6 +31,9 @@ public class LocationServiceImpl implements LocationService {
 
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private DriverInfoFeignClient driverInfoFeignClient;
 
     @Override
     public Boolean updateDriverLocation(UpdateDriverLocationForm updateDriverLocationForm) {
@@ -45,16 +56,49 @@ public class LocationServiceImpl implements LocationService {
 
         // 1. 操作redis里面geo
         // 1.1 穿件circle对象
-        Circle circle = null;
-        // 1.2 定义GEO参数
-        RedisGeoCommands.GeoRadiusCommandArgs args = null;
-        GeoResults<RedisGeoCommands.GeoLocation<String>> radius = redisTemplate.opsForGeo().radius(RedisConstant.DRIVER_GEO_LOCATION, circle, args);
+        Point point = new Point(searchNearByDriverForm.getLongitude().doubleValue(), searchNearByDriverForm.getLatitude().doubleValue());
+        Distance distance = new Distance(SystemConstant.NEARBY_DRIVER_RADIUS, RedisGeoCommands.DistanceUnit.KILOMETERS);        // 1.2 定义GEO参数
+        Circle circle = new Circle(point, distance);
+        RedisGeoCommands.GeoRadiusCommandArgs args = RedisGeoCommands.GeoRadiusCommandArgs.newGeoRadiusArgs()
+                .includeDistance() // 包含距离
+                .includeCoordinates() // 包含坐标
+                .sortAscending(); // 升序
+        GeoResults<RedisGeoCommands.GeoLocation<String>> result = redisTemplate.opsForGeo().radius(RedisConstant.DRIVER_GEO_LOCATION, circle, args);
         // 2. 查询redis最后返回list集合
-
+        List<GeoResult<RedisGeoCommands.GeoLocation<String>>> content = result.getContent();
         // 3. 对查询list集合进行处理
         // 3.1 遍历list集合，得到每个司机信息
-        // 3.2 根据每个司机个性化设置信息判断
-        return null;
+        ArrayList<NearByDriverVo> list = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(content)) {
+            Iterator<GeoResult<RedisGeoCommands.GeoLocation<String>>> iterator = content.iterator();
+            while (iterator.hasNext()) {
+                GeoResult<RedisGeoCommands.GeoLocation<String>> next = iterator.next();
+                // 3.1.1 得到司机id
+                Long driverId = Long.valueOf(next.getContent().getName());
+                // 3.2 根据每个司机个性化设置信息判断
+                Result<DriverSet> driverSetResult = driverInfoFeignClient.getDriverSet(driverId);
+                DriverSet driverSet = driverSetResult.getData();
+                //  3.2.1 判断订单鲤城
+                BigDecimal orderDistance = driverSet.getOrderDistance();
+                //  3.2.2 判断订单距离是否超过司机设置
+                if (orderDistance.doubleValue() != 0 && orderDistance.subtract(searchNearByDriverForm.getMileageDistance()).doubleValue() < 0) {
+                    continue;
+                }
+                // 3.2.3 判断接单里程
+                BigDecimal currentDistance = new BigDecimal(next.getDistance().getValue()).setScale(2, RoundingMode.HALF_UP);
+                BigDecimal acceptDistance = driverSet.getAcceptDistance();
+                if (acceptDistance.doubleValue() != 0 && acceptDistance.subtract(currentDistance).doubleValue() < 0) {
+                    continue;
+                }
+
+                // 封装符合条件数据
+                NearByDriverVo nearByDriverVo = new NearByDriverVo();
+                nearByDriverVo.setDriverId(driverId);
+                nearByDriverVo.setDistance(currentDistance);
+                list.add(nearByDriverVo);
+            }
+        }
+        return list;
     }
 
 }
